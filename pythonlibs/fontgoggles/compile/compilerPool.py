635215c82f883b8507ff4da98f59517a962b0cda
toby5001 <andrewlouda@gmail.com>
2025-08-28 17:47:00 -0500

    Disabled call to unsupported functions in fontgoggles on load. Not at all a bulletproof fix but this is hopefully only needed temporarily.

--- pythonlibs/fontgoggles/compile/compilerPool.py
+++ pythonlibs/fontgoggles/compile/compilerPool.py
@@ -1,26 +1,26 @@
 import os
 import tempfile
 
-from .ufoCompiler import compileUFOToPath as ufo_compileUFOToPath
+# from .ufoCompiler import compileUFOToPath as ufo_compileUFOToPath
 from .dsCompiler import compileDSToPath as ds_compileDSToPath
 from .ttxCompiler import compileTTXToPath as ttx_compileTTXToPath
 
-def compileUFOToPath(ufoPath, ttPath, outputWriter):
-    return ufo_compileUFOToPath( os.fspath(ufoPath), os.fspath(ttPath), )
+# def compileUFOToPath(ufoPath, ttPath, outputWriter):
+#     return ufo_compileUFOToPath( os.fspath(ufoPath), os.fspath(ttPath), )
 
-# FIXME: Ok so while the ufo doesn't error anymore and some of the font is created, the actual glyph paths appear to be empty.
-def compileUFOToBytes(ufoPath, outputWriter):
-    tmp = tempfile.NamedTemporaryFile(prefix="fontgoggles_temp", suffix=".ttf")
-    tmp.close()
-    compileUFOToPath(ufoPath, tmp.name, outputWriter)
-    with open(tmp.name, "rb") as f:
-        fontData = f.read()
-        if not fontData:
-            fontData = None
-    if tmp:
-        tmp.close()
-        os.unlink(tmp.name)
-    return fontData
+# # FIXME: Ok so while the ufo doesn't error anymore and some of the font is created, the actual glyph paths appear to be empty.
+# def compileUFOToBytes(ufoPath, outputWriter):
+#     tmp = tempfile.NamedTemporaryFile(prefix="fontgoggles_temp", suffix=".ttf")
+#     tmp.close()
+#     compileUFOToPath(ufoPath, tmp.name, outputWriter)
+#     with open(tmp.name, "rb") as f:
+#         fontData = f.read()
+#         if not fontData:
+#             fontData = None
+#     if tmp:
+#         tmp.close()
+#         os.unlink(tmp.name)
+#     return fontData
 
 
 def compileDSToPath(dsPath, fontNumber, ttFolder, ttPath, outputWriter):
