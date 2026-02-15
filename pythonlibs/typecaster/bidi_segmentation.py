0192315ed4bf2596e8e9aa55ffd8cf12acbea8d1
toby5001 <andrewlouda@gmail.com>
2026-02-14 22:52:21 -0600

    Added per-run directional information to run_info being output

--- pythonlibs/typecaster/bidi_segmentation.py
+++ pythonlibs/typecaster/bidi_segmentation.py
@@ -90,7 +90,7 @@ def line_to_run_segments(line_text, run_id_current=0, run_info=[]):
         segmentation.reverse()
 
     for value, sub in segmentation:
-        run_info.append( (run_id_current, value%2) )
+        run_info.append( (run_id_current, value%2, isRTL) )
         run_id_current += 1
         # This method is not the same as what fontgoggles does, but I've diverged pretty significantly at this point so maybe that's alright
         if value%2:
